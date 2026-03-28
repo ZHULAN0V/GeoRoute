e5082b1f795bd2224fac3920869b27e1f3b26708
Ivan Kropotin <kropotiniv@yandex.ru>
2026-03-28 20:36:23 +0500

    Прикрутил S3

---
+++ georoute-backend/src/main/java/org/urfu/georoute/service/impl/MinioService.java
@@ -0,0 +1,108 @@
+package org.urfu.georoute.service.impl;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import jakarta.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import org.urfu.georoute.service.FileStorageService;
+
+import io.minio.BucketExistsArgs;
+import io.minio.GetObjectArgs;
+import io.minio.ListObjectsArgs;
+import io.minio.MakeBucketArgs;
+import io.minio.MinioClient;
+import io.minio.PutObjectArgs;
+import io.minio.RemoveObjectArgs;
+import io.minio.Result;
+import io.minio.errors.MinioException;
+import io.minio.messages.Item;
+
+@Service
+public class MinioService implements FileStorageService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MinioService.class);
+
+    private final MinioClient minioClient;
+    private final String bucketName;
+
+    public MinioService(MinioClient minioClient, @Value("${minio.bucket.name}") String bucketName) {
+        this.minioClient = minioClient;
+        this.bucketName = bucketName;
+    }
+
+    @PostConstruct
+    public void init() {
+        try {
+            boolean doesBucketExist = minioClient.bucketExists(BucketExistsArgs.builder().bucket(bucketName).build());
+            if (!doesBucketExist) {
+                minioClient.makeBucket(MakeBucketArgs.builder().bucket(bucketName).build());
+            }
+        } catch (MinioException e) {
+            LOGGER.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public String saveFile(MultipartFile file) throws IOException {
+        String filename = file.getOriginalFilename();
+        try {
+            minioClient.putObject(
+                PutObjectArgs.builder()
+                    .bucket(bucketName)
+                    .object(filename)
+                    .stream(file.getInputStream(), file.getSize(), -1L)
+                    .contentType(file.getContentType())
+                    .build()
+            );
+        } catch (MinioException e) {
+            throw new IOException(e);
+        }
+        return filename;
+    }
+
+    @Override
+    public InputStream getFile(String fileName) throws IOException {
+        try {
+            return minioClient.getObject(
+                GetObjectArgs.builder()
+                    .bucket(bucketName)
+                    .object(fileName)
+                    .build()
+            );
+        } catch (MinioException e) {
+            throw new IOException(e);
+        }
+    }
+
+    @Override
+    public Iterable<Result<Item>> getAllFiles() {
+        return minioClient.listObjects(
+            ListObjectsArgs.builder()
+                .bucket(bucketName)
+                .recursive(true)
+                .build()
+        );
+    }
+
+    @Override
+    public void deleteFile(String fileName) throws IOException {
+        try {
+            minioClient.removeObject(
+                RemoveObjectArgs.builder()
+                    .bucket(bucketName)
+                    .object(fileName)
+                    .build()
+            );
+        } catch (MinioException e) {
+            throw new IOException(e);
+        }
+    }
+
+}
